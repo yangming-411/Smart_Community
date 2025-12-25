6cf92e7ed6539caad0b9ed7a7f14f35f3fdc003b
yangming-411 <2023177024@qq.com>
2025-12-25 18:51:47 +0800

    feat:添加力控制器配置

--- src/smart_community_sim/launch/gazebo_sim.launch.py
+++ src/smart_community_sim/launch/gazebo_sim.launch.py
@@ -51,7 +51,10 @@ def generate_launch_description():
     action_load_joint_state_controller = launch.actions.ExecuteProcess(
         cmd='ros2 control load_controller robot_joint_state_broadcaster --set-state active'.split(' '),
         output='screen'
-
+    )
+    action_load_effort_state_controller = launch.actions.ExecuteProcess(
+        cmd='ros2 control load_controller robot_effort_controller --set-state active'.split(' '),
+        output='screen'
     )
     
     return launch.LaunchDescription([
@@ -64,5 +67,11 @@ def generate_launch_description():
                 target_action=action_spawn_entity,
                 on_exit=[action_load_joint_state_controller],
             )
-        )
+        ),
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=action_load_joint_state_controller,
+                on_exit=[action_load_effort_state_controller],
+            )
+        ),
     ])
